7d2187754a237a836a53f8cdc88a427a0af2fe6f
smartjeff532 <smartjeff532@gmail.com>
2024-11-14 12:30:43 +0300

    Develop -  Where my Reducers are confirgured

---
+++ src/app/redux/reducers.ts
@@ -0,0 +1,32 @@
+import { ADD_ITEM, REMOVE_ITEM } from './actions';
+
+interface CartState {
+  cartItems: Record<string, number>;
+}
+
+const initialState: CartState = {
+  cartItems: {},
+};
+
+const cartReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case ADD_ITEM:
+      return {
+        ...state,
+        cartItems: {
+          ...state.cartItems,
+          [action.payload]: (state.cartItems[action.payload] || 0) + 1,
+        },
+      };
+    case REMOVE_ITEM:
+      const { [action.payload]: _, ...newCartItems } = state.cartItems;
+      return {
+        ...state,
+        cartItems: newCartItems,
+      };
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
