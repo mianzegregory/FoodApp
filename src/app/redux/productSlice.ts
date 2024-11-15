060f1a3202525a8b68b57dd63a44677a9abe5da0
smartjeff532 <smartjeff532@gmail.com>
2024-11-15 09:09:53 +0300

    Develop - Removed the products from my productslice to product.json

--- src/app/redux/productSlice.ts
+++ src/app/redux/productSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import productsData from '@/app/products.json'; 
 
 interface Product {
   id: number;
@@ -11,28 +12,10 @@ interface ProductState {
   products: Product[];
 }
 
-
-const image = {
-  apple: '/images/apple.jpg',
-  avocado: '/images/avacado.jpg',
-  burger: '/images/burger.jpg',
-  hotdog: '/images/hotdog.jpg',
-  melon: '/images/melon.jpg',
-  pizza: '/images/pizaa.jpg',
-  salad: '/images/salad.jpg',
-};
-
 const initialState: ProductState = {
-  products: [
-    { id: 1, name: 'Apple', price: 50, image: image.apple }, 
-    { id: 2, name: 'Avacado', price: 30, image: image.avocado }, 
-    { id: 3, name: 'Burger', price: 80, image: image.burger },
-    { id: 4, name: 'Hotdog', price: 100, image: image.hotdog },
-    { id: 5, name: 'Melon', price: 120, image: image.melon },
-    { id: 6, name: 'Pizaa', price: 800, image: image.pizza }, 
-    { id: 7, name: 'Salad', price: 50, image: image.salad },
-  ],
+  products: productsData, 
 };
+
 const productSlice = createSlice({
   name: 'products',
   initialState,
