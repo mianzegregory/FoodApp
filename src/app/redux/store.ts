601cc289bab78bcb5575401729b8dbcb2b2effb5
smartjeff532 <smartjeff532@gmail.com>
2024-11-15 10:16:25 +0300

    Develop -  my store.ts

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,16 +1,12 @@
 
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cartSlice'; 
-import productReducer from './productSlice'; 
+import cartReducer from 'C:/Users/Smart/Desktop/app/nextjs-dashboard/app/state/cart/cartSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
-    products: productReducer, 
   },
 });
-
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
 
 export default store;
