84925973a7fee35b2a2d113e4e5e0276a470dc3a
smartjeff532 <smartjeff532@gmail.com>
2024-11-15 10:31:18 +0300

    Develop- Removed import home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import store from '@/app/redux/store'; 
-import home from '@/app/Home/page'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
